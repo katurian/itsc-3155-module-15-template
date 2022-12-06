1160027422789ac55bcda12983a022bed71c3ec2
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2022-12-06 02:03:50 -0500

    Update movie_repository.py

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -3,21 +3,21 @@ from src.models import Movie, db
 
 class MovieRepository:
 
-    def get_all_movies(self) -> list[Movie]:
+    def get_all_movies(self):
         all_movies: list[Movie] = Movie.query.all()
         return all_movies
 
-    def get_movie_by_id(self, movie_id: int) -> Movie:
+    def get_movie_by_id(self, movie_id: int):
         found_movie: Movie = Movie.query.get_or_404(movie_id)
         return found_movie
 
-    def create_movie(self, title: str, director: str, rating: int) -> Movie:
+    def create_movie(self, title: str, director: str, rating: int):
         new_movie = Movie(title=title, director=director, rating=rating)
         db.session.add(new_movie)
         db.session.commit()
         return new_movie
 
-    def search_movies(self, title) -> list[Movie]:
+    def search_movies(self, title):
         found_movies: list[Movie] = Movie.query.filter(Movie.title.ilike(f'%{title}%')).all()
         return found_movies
 
